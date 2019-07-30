6e20e98b961fe4664d72859a55c7da9b821d31a6
smpatel89 <sahil.patel@verizon.com>
2019-07-30 11:23:47 -0400

    try

--- 08-try04.py
+++ 08-try04.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+import uuid
 
+ticket = uuid.uuid1()
+
+try:
+    print('Please type the config file to load')
+    config_file = input('filename: ')
+    config_file_obj = open('data/'+config_file, 'r')
+    switch_config = config_file_obj.read()
+except:
+    x = 'Error with obtaining config file info'
+else:
+    x = 'Switch file found'
+finally:
+    close(config_file_obj)
+    with open('data/try04.log', 'a') as zlog:
+        print('\n\nWriting results to log')
+        print(ticket, ' - ', x, file=zlog)
+    
