74f4edef1fb935a54a179f1ef9a4da418c14a1e0
smpatel89 <sahil.patel@verizon.com>
2019-08-01 09:23:04 -0400

    btc2

--- 15-proj.py
+++ 15-proj.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
 
 from flask import Flask, request
-import crayons
+import argparse
 import requests
 
 app = Flask(__name__)
-API_KEY='RYSS5B71Y18GH26A'
-API_ENDPOINT='https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_WEEKLY&symbol=BTC&market=CNY&apikey={}'
+API_KEY=None
+API_ENDPOINT='https://www.alphavantage.co/query?function={}&apikey={}'
 
 @app.route('/btc_weekly')
 def btc_weekly():
-    print(API_ENDPOINT.format(API_KEY))
     try:
-        req = requests.get(API_ENDPOINT.format(API_KEY))
+        req = requests.get( API_ENDPOINT.format('DIGITAL_CURRENCY_WEEKLY&symbol=BTC&market=USD', API_KEY))
         data = req.json()['Time Series (Digital Currency Weekly)']
     except Exception as e:
         return {'status': f'Failed - {str(e)}'}
     else:
-        for key in data.keys():
-            del data[key]['1a. open (CNY)']
-            del data[key]['2a. high (CNY)']
-            del data[key]['3a. low (CNY)']
-            del data[key]['4a. close (CNY)']
+        #for key in data.keys():
+        #    del data[key]['1a. open (CNY)']
+        #    del data[key]['2a. high (CNY)']
+        #    del data[key]['3a. low (CNY)']
+        #    del data[key]['4a. close (CNY)']
+        return data
+
+@app.route('/btc_current')
+def btc_current():
+    try:
+        req = requests.get(API_ENDPOINT.format('CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD',API_KEY))
+        data = req.json()['Realtime Currency Exchange Rate']
+    except Exception as e:
+        return {'status': f'Failed - {str(e)}'}
+    else:
         return data
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key', type=str, help='Alpha Vantage API Key')
+    args = parser.parse_args();
+    API_KEY = args.key
+    print(f'Using key {API_KEY}')
+
     app.run(port=5006, debug=True)
